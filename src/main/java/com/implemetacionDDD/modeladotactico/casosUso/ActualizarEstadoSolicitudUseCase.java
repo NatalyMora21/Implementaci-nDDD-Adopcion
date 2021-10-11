f25553fcc2bcaf8a9fb6ad9ece4d21b970f85c06
NatalyMora21 <tatamora21gmail.com>
2021-10-11 01:21:33 -0500

    Creado caso de usoo Actualizar estado de la solicitud

--- src/main/java/com/implemetacionDDD/modeladotactico/casosUso/ActualizarEstadoSolicitudUseCase.java
+++ src/main/java/com/implemetacionDDD/modeladotactico/casosUso/ActualizarEstadoSolicitudUseCase.java
@@ -1,4 +1,18 @@
 package com.implemetacionDDD.modeladotactico.casosUso;
 
-public class ActualizarEstadoSolicitudUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import com.implemetacionDDD.modeladotactico.entity.solicitudAdopcion.Command.ActualizarEstadoSolicitud;
+import com.implemetacionDDD.modeladotactico.entity.solicitudAdopcion.SolicitudAdopcion;
+
+public class ActualizarEstadoSolicitudUseCase extends UseCase <RequestCommand <ActualizarEstadoSolicitud>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<ActualizarEstadoSolicitud> actualizarEstadoSolicitudRequestCommand) {
+        var command = actualizarEstadoSolicitudRequestCommand.getCommand();
+        var solicitudAdopcion = SolicitudAdopcion.from(command.getSolicitudAdopcionId(),retrieveEvents(command.getSolicitudAdopcionId().value()));
+        solicitudAdopcion.actualizarEstado(command.getEstadoSolicitudAdopcion());
+        emit().onResponse(new ResponseEvents(solicitudAdopcion.getUncommittedChanges()));
+    }
 }
